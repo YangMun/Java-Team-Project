bf9e3d00f4b59c4879bcbab379e3df3aad4b9016
labor_book <labor_book@10.211.55.12>
2021-11-20 14:11:21 +0900

    2021.11.20 기본 틀 업데이트

---
+++ src/Project01/SearchProgram.java
@@ -0,0 +1,52 @@
+package Project01;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class SearchProgram extends JFrame{
+	private String colNames[] = {"이름", "주소", "전화번호"};
+	
+	private JTextField nameText = new JTextField(5);
+	private JTextField addressText = new JTextField(8);
+	private JTextField phonenoText = new JTextField(5);
+	
+	public SearchProgram() {
+		setTitle("연락처 검색 프로그램");
+		setPreferredSize(new Dimension(450,200));
+		setLocation(500, 400);
+		Container c = getContentPane();
+		
+		DefaultTableModel model = new DefaultTableModel(colNames, 0);
+		JTable table = new JTable(model);
+		c.add(new JScrollPane(table), BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		
+		JButton findButton = new JButton("검색");
+		
+		panel.add(new JLabel("이름"));
+		panel.add(nameText);
+		
+		panel.add(new JLabel("주소"));
+		panel.add(addressText);
+		
+		panel.add(new JLabel("전화번호"));
+		panel.add(phonenoText);
+		
+		panel.add(findButton);
+
+		c.add(panel, BorderLayout.NORTH);
+		
+		//find.addActionListener(new AddActionListener(table, nameText, addressText, phonenoText));
+	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new SearchProgram();
+	}
+
+}
---
+++ src/Project02/Manager.java
@@ -0,0 +1,55 @@
+package Project02;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class Manager extends JFrame{
+	private String colNames[] = {"이름", "주소", "전화번호"};
+	
+	private JTextField nameText = new JTextField(5);
+	private JTextField addressText = new JTextField(8);
+	private JTextField phonenoTextText = new JTextField(5);
+	
+	public Manager() {
+		setTitle("학생정보관리 프로그램");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(700,200));
+		setLocation(500, 200);
+		Container c = getContentPane();
+		
+		DefaultTableModel model = new DefaultTableModel(colNames, 0);
+		JTable table = new JTable(model);
+		c.add(new JScrollPane(table), BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		
+		JButton select = new JButton("검색"); // select 버튼
+		JButton insert = new JButton("추가"); // insert 버튼
+		JButton update = new JButton("변경"); // select 버튼
+		JButton delete = new JButton("삭제"); // delete 버튼
+		
+		panel.add(new JLabel("이름"));
+		panel.add(nameText);
+		
+		panel.add(new JLabel("주소"));
+		panel.add(addressText);
+		
+		panel.add(new JLabel("전화번호"));
+		panel.add(phonenoTextText);
+		
+		panel.add(select);
+		panel.add(insert);
+		panel.add(update);
+		panel.add(delete);
+		
+		c.add(panel, BorderLayout.NORTH);
+		pack();
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Manager();
+	}
+
+}
