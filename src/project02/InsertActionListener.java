2eb7fd2d090556c9cb03b80cc792977abcc101d9
tunas77s <94696416+tunas77s@users.noreply.github.com>
2021-11-22 23:43:16 +0900

    insert부분 수정완료

--- src/project02/InsertActionListener.java
+++ src/project02/InsertActionListener.java
@@ -13,7 +13,7 @@ public class InsertActionListener implements ActionListener {
 	private ResultSet rs = null;
 
 	DBConnection con = new DBConnection();
-	JTextField name, address, phoneno; //JTextField 생성
+	JTextField jumin, name, address, phoneno; //JTextField 생성
 	JTextField text[];
 	DefaultTableModel model; //model 가져와 하나로 공유하기 위함
 
@@ -25,16 +25,18 @@ public class InsertActionListener implements ActionListener {
 	        try {
 	        	Connection conn = con.getDBConn();
 	        	
-	        	name = text[0];
-	        	address = text[1];
-	        	phoneno = text[2];
+	        	jumin = text[0];
+	        	name = text[1];
+	        	address = text[2];
+	        	phoneno = text[3];
 
-	        	String sql= "INSERT INTO studentinfo VALUES(?, ?, ?)"; 
+	        	String sql= "INSERT INTO studentinfo VALUES(?, ?, ?, ?)"; 
 		       	pstmt=conn.prepareStatement(sql); 
-     	
-		       	pstmt.setString(1, name.getText()); //name으로 들어감
-		        pstmt.setString(2, address.getText()); //address로 들어감
-		        pstmt.setString(3, phoneno.getText()); //phoneno로 들어감
+		       	
+		       	pstmt.setString(1, jumin.getText()); //jumin으로 들어감
+		       	pstmt.setString(2, name.getText()); //name으로 들어감
+		        pstmt.setString(3, address.getText()); //address로 들어감
+		        pstmt.setString(4, phoneno.getText()); //phoneno로 들어감
 
 		        pstmt.executeUpdate();
 		        new ModelPrint(model);
