2e0b64ce7a761227590cd8150d548fe5eabcf61d
jungo <jungo@DESKTOP-GSGL180>
2021-11-22 22:23:13 +0900

    delete

---
+++ src/project02/deleteActionListener.java
@@ -0,0 +1,51 @@
+package project02;
+
+import com.ysu.dbconnection.DBConnection;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+
+public class deleteActionListener implements ActionListener {
+	private Connection conn=null;
+	private PreparedStatement pstmt=null;
+	private ResultSet rs = null;
+
+	DBConnection con = new DBConnection();
+	JTextField name; 
+	JTextField text[];
+	DefaultTableModel model; 
+
+	deleteActionListener(DefaultTableModel model, JTextField text[]) {
+		this.model = model;
+		this.text = text;
+		}
+		public void actionPerformed(ActionEvent e) {
+	        try {
+	        	Connection conn = con.getDBConn();
+	        	
+	        	name = text[0];
+
+	        	String sql= "DELETE FROM studentinfo WHERE name=?"; 
+		       	pstmt=conn.prepareStatement(sql); 
+     	
+		       	pstmt.setString(1, name.getText());
+
+		        pstmt.executeUpdate();
+		        new ModelPrint(model);
+
+	        }  
+	        catch(SQLException se) {
+	        	System.out.println(se.getMessage());
+	        }
+	        finally{
+		       	 if(rs != null) 
+		       	    try{rs.close();}catch(SQLException sqle){}
+		       	 if(pstmt != null) 
+		       		try{pstmt.close();}catch(SQLException sqle){}
+		       	 if(conn != null) 
+		       		try{conn.close();}catch(SQLException sqle){}
+		         }
+	    }    
+}
