5aa3714732e14356a095acf3bc667df4ed5d6f57
labor_book <labor_book@10.211.55.12>
2021-11-24 00:34:18 +0900

    ValueCheck.java에 주석문 추가

--- src/project02/ValueCheck.java
+++ src/project02/ValueCheck.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 import javax.swing.JTextField;
 
 public class ValueCheck {
-	private String regJumin ="^\\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|[3][01])\\-[1-4][0-9]{6}$";
-	private String regex = "^0[123456][016789]?-\\d{4}-\\d{4}$";
-	private String regName = "^[가-힣]+|[a-zA-Z]+$";
+	private String regJumin ="^\\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|[3][01])\\-[1-4][0-9]{6}$"; // 주민번호에 대한 정규식
+	private String regPhone = "^0[123456][016789]?-\\d{4}-\\d{4}$"; // 휴대전화에 대한 정규식
+	private String regName = "^[가-힣]+|[a-zA-Z]+$"; // 이름에 대한 정규식
 private static ValueCheck instance = new ValueCheck();
     
     public static ValueCheck getInstance() {
@@ -18,7 +18,7 @@ private static ValueCheck instance = new ValueCheck();
     
     public int phoneCheck(JTextField phoneno) {
     	int x = 0;
-    	 if(Pattern.matches(regex, (CharSequence) phoneno.getText()) == true) // 전화번호 정규식 적용
+    	 if(Pattern.matches(regPhone, (CharSequence) phoneno.getText()) == true) // 전화번호 정규식 적용
 	        {
     		 x = 1;
 	        }
